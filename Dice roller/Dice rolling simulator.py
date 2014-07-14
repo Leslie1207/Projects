30c42f0aab3d04613271452d31f1b6e412df8ff7
Leslie Mathys <leslie1207@hotmail.co.uk>
2014-07-14 01:47:52 +0100

    Clean up
    Removal of while loop from dice_roll function, as there was no need for
    it.
    
    Addition of while loop to roll_dice_again function, as it originally
    called functions within itself, which will deplete memory.
    
    Changes made by ‘anossov’ on reddit.

--- Dice roller/Dice rolling simulator.py
+++ Dice roller/Dice rolling simulator.py
@@ -1,45 +1,44 @@
 """ Dice rolling simulator, will ask user to press Enter to roll.
 	Will print a random number between 1 to 6. Will ask user if they want to roll again. """
-	
+ 
 import random
-
+ 
 print "Welcome to the Dice Rolling Simulator!\n"
 print "Press Enter to roll the dice."
 begin = raw_input("")
-
+ 
 """ function where random numbers are generated """
 def dice_roll():
-	while True:
-		print "You have rolled..."
-		print random.randint(1,6)
-		print "and"
-		print random.randint(1,6)
-		break
-		
+	# A while that does one iteration is equivalent to no while at all
+	print "You have rolled..."
+	print random.randint(1,6)
+	print "and"
+	print random.randint(1,6)
 	print "\n"
-
+ 
 """ ask whether player wants to roll the dice again. will only accept yes or no. 
 	otherwise loops back. """
-def roll_dice_again():	
-	again = str(raw_input("Would you like to roll the dice again? 'Yes' or 'No'?\n").lower())
-		
-	if again == "yes": 
-		print "\n"
-		dice_roll()
-		roll_dice_again()
+def roll_dice_again():
+	while True:
+		again = str(raw_input("Would you like to roll the dice again? 'Yes' or 'No'?\n").lower())
+	
+		if again == "yes": 
+			print "\n"
+			dice_roll()
 			
-	elif again == "no":
-		print "\n"
-		print "Thank you for playing."
+		elif again == "no":
+			print "\n"
+			print "Thank you for playing."
+			break  # exit while. `return` can also be used here to exit both `while` and `roll_dice_again`
 		
-	else:
-		print "\n"
-		print "That is incorrect."
-		roll_dice_again()
-
+		else:
+			print "\n"
+			print "That is incorrect."
+			
+		# no `break` happened, continue looping
+ 
 dice_roll()
-roll_dice_again()	
-
+roll_dice_again()
 
 
 
