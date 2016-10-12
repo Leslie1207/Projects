8380b644bc122dea5b3b6a37d5fdf736b46820ef
LM <leslie1207@hotmail.co.uk>
2016-10-12 18:32:35 +0100

    Reddit suggestions

--- QB Rating/QB Rating.py
+++ QB Rating/QB Rating.py
@@ -14,31 +14,33 @@ comp = float(input("Please input the number of PASSING COMPLETIONS: "))
 yds = float(input("Please input the number of PASSING YARDS: "))
 td = float(input("Please input the number of TOUCHDOWN PASSES: "))
 inter = float(input("Please input the number of INTERCEPTIONS: "))
+maxRating = 2.375
+minRating = 0
 
 def qbRating (att, comp, yds,td, inter):
     a = (((comp/att)-0.3) * 5)
     b = (((yds/att)-3) * 0.25)
     c = ((td/att) * 20)
     d = (2.375 - ((inter/att) * 25))   
-    if a > 2.375:
-        a = 2.375
-    if a < 0:
-        a = 0
-    if b > 2.375:
-        b = 2.375
-    if b < 0:
-        b = 0
-    if c > 2.375:
-        c = 2.375
-    if c < 0:
-        c = 0
-    if d > 2.375:
-        d = 2.375
-    if d < 0:
-        d = 0       
+    if a > maxRating:
+        a = maxRating
+    if a < minRating:
+        a = minRating
+    if b > maxRating:
+        b = maxRating
+    if b < minRating:
+        b = minRating
+    if c > maxRating:
+        c = maxRating
+    if c < minRating:
+        c = minRating
+    if d > maxRating:
+        d = maxRating
+    if d < minRating:
+        d = minRating       
     passerRating = ((a + b + c + d)/6) * 100
     return round(passerRating, 1)
     
 
-print(("\n%s's QB rating is: ") %(qbName))
-print(qbRating(att, comp, yds, td, inter)) 
+print(("\n%s's QB rating is: ") %(qbName), qbRating(att, comp, yds, td, inter)) 
+
