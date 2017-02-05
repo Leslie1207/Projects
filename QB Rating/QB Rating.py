70806d175bdc410fd4704a2bbd9da3ed504ed0b5
LM <leslie1207@hotmail.co.uk>
2017-02-05 10:05:30 +0000

    Tidied up

--- QB Rating/QB Rating.py
+++ QB Rating/QB Rating.py
@@ -14,33 +14,23 @@ comp = float(input("Please input the number of PASSING COMPLETIONS: "))
 yds = float(input("Please input the number of PASSING YARDS: "))
 td = float(input("Please input the number of TOUCHDOWN PASSES: "))
 inter = float(input("Please input the number of INTERCEPTIONS: "))
-maxRating = 2.375
-minRating = 0
+
+def clamp(x, min_n, max_n):
+    return min(max(x, min_n), max_n)
 
 def qbRating (att, comp, yds,td, inter):
     a = (((comp/att)-0.3) * 5)
     b = (((yds/att)-3) * 0.25)
     c = ((td/att) * 20)
     d = (2.375 - ((inter/att) * 25))   
-    if a > maxRating:
-        a = maxRating
-    if a < minRating:
-        a = minRating
-    if b > maxRating:
-        b = maxRating
-    if b < minRating:
-        b = minRating
-    if c > maxRating:
-        c = maxRating
-    if c < minRating:
-        c = minRating
-    if d > maxRating:
-        d = maxRating
-    if d < minRating:
-        d = minRating       
-    passerRating = ((a + b + c + d)/6) * 100
-    return round(passerRating, 1)
-    
+
+    a = clamp(a, min_n=0, max_n=2.375)
+    b = clamp(b, min_n=0, max_n=2.375) 
+    c = clamp(c, min_n=0, max_n=2.375)
+    d = clamp(d, min_n=0, max_n=2.375)
+
+    return(a)
+    return round(((a + b + c + d)/6) * 100, 1)
 
 print(("\n%s's QB rating is: ") %(qbName), qbRating(att, comp, yds, td, inter)) 
 
