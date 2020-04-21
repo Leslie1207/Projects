2862c24350624332dab5e3f6a4dab86227c2484a
Leslie Mathys <leslie1207@hotmail.co.uk>
2020-04-21 13:15:09 +1000

    Updated to pull semsnsitive information from user variable.

--- VEVO Check Automator/vevo check.py
+++ VEVO Check Automator/vevo check.py
@@ -1,70 +1,73 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 import time
+import os
 
-dob = ""
-passportnumber = ("")
-email = ""
+# Using os module to get sensitive data from user variable
+dob = os.environ.get("VEVO_DOB")
+passportnumber = os.environ.get("VEVO_PPT")
+email = os.environ.get("VEVO_EMAIL")
+visagrantnumber = os.environ.get("VEVO_GRANT")
 
-#Storing path location of Opera browser driver in variable to pass through as argument below:
-chromedriver = "C:\\Users\\lesli\\AppData\\Local\\Programs\\Python\\Python38-32\\Lib\\site-packages\\selenium\\webdriver\\chrome\\chromedriver.exe"
-driver = webdriver.Chrome(executable_path = chromedriver)
-#Telling Chrome to open Immi website.
+chromedriver = (
+    "C:\\Users\\lesli\\AppData\\Local\\Programs\\Python\\Python38-32\\Lib\\site-packages\\selenium\\webdriver\\chrome\\chromedriver.exe")
+driver = webdriver.Chrome(executable_path=chromedriver)
+# Telling Chrome to open Immi website.
 driver.get("https://online.immi.gov.au/evo/firstParty?actionType=query")
 
-#Use Select to open drop-down to choose document type
+# Use Select to open drop-down to choose document type
 documenttype = Select(driver.find_element_by_id("_2a0a2a0a2a0a1a_input"))
 time.sleep(2)
-#Select Passport
+# Select Passport
 documenttype.select_by_visible_text("Passport")
 time.sleep(2)
 
-#Use Select to open drop-down too choose reference type
+# Use Select to open drop-down too choose reference type
 referencetype = Select(driver.find_element_by_id("_2a0a2a0a2c1a0b_input"))
 time.sleep(2)
-#Select Visa Grant Number
+# Select Visa Grant Number
 referencetype.select_by_visible_text("Visa Grant Number")
 
-#Wait for 5 secs, gives next dropdown chance to open
+# Wait for 5 secs, gives next dropdown chance to open
 time.sleep(2)
 
-#Find Visa Grant Number box
+# Find Visa Grant Number box
 referencetypenumber = driver.find_element_by_id("_2a0a2a0a2c1b1b0_input")
-#Insert Visa Grant Number
+# Insert Visa Grant Number
 referencetypenumber.send_keys(visagrantnumber)
 
-#Find DOB entry box
+# Find DOB entry box
 dobinsert = driver.find_element_by_id("_2a0a2a0a2e0a1a_input")
-#Insert DOB into box
+# Insert DOB into box
 dobinsert.send_keys(dob)
 
-#Find passport number entry box
+# Find passport number entry box
 passportinsert = driver.find_element_by_id("_2a0a2a0a2e0b1a_input")
-#Insert passport number
+# Insert passport number
 passportinsert.send_keys(passportnumber)
 
-#Find country drop-down box
+# Find country drop-down box
 country = Select(driver.find_element_by_id("_2a0a2a0a2e0c1a_input"))
-#Select country
+# Select country
 country.select_by_visible_text("UNITED KINGDOM - BRITISH CITIZEN")
 
-#Find checkbox that agrees to terms and conditions
+# Find checkbox that agrees to terms and conditions
 checkbox = driver.find_element_by_id("_2a0a2a0a2f1b0_input").click()
 
-#Find 'Submit' button and click
+# Find 'Submit' button and click
 submit = driver.find_element_by_id("_2a0a2a0a3b0a").click()
 time.sleep(2)
 
-#Send VEVO check as PDF
+# Send VEVO check as PDF
 sendemail = driver.find_element_by_id("_2a3a2a0a2c1b0").click()
 
-#Enter email first time
+# Enter email first time
 inputemail1 = driver.find_element_by_id("_2a5a2a0a2a0b0_input")
 inputemail1.send_keys(email)
 
-#Repeat email for verification
+# Repeat email for verification
 inputemail2 = driver.find_element_by_id("_2a5a2a0a2a1b0_input")
 inputemail2.send_keys(email)
 
-#Press send button
+# Press send button
 sendvevo = driver.find_element_by_id("_2a5a2a0a3b0a").click()
